9e2bbb9743ffd4753986684718e1dc46cdcad1d1
swati.jain <swati.jain@tothenew.com>
2017-04-29 13:21:28 +0530

    Connecting to server and fetching data

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var express = require('express')
-var mongoose = require('mongoose')
+var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
-
 const mongoUrl = require('./server/config/db');
+const net = require('net');
+const clientTitle = process.argv[2] || "Client-"+Date.now();
+const resource = process.argv[3] || 'meetup';
+let startTime = null;
+
+const Buffer = require('buffer');
 
 mongoose.connect(mongoUrl.mongoUrl);
 
@@ -18,3 +23,30 @@ mongoose.connection.on("connected",() => {
 app.listen(3000, function () {
   console.log('App is running on 3000!')
 })
+
+const client = net.connect({port: 3000}, () => {
+    // 'connect' listener
+    console.log(`connected to server! - \nClient Name - ${clientTitle}\nResource - ${resource}` );
+    client.write(JSON.stringify({action: 'subscribe', type: resource}));
+    // client.write(JSON.stringify({action: 'subscribe', type: 'fbpost'}));
+    client.write(JSON.stringify({action: 'subscribe', type: 'tweet'}));
+    startTime = Date.now();
+    /*setTimeout(() => {
+        client.write(JSON.stringify({action: 'subscribe', type: 'fbpost'}));
+    }, 1000);
+
+    setTimeout(() => {
+        // client.write(JSON.stringify({action: 'subscribe', type: 'fbpost'}));
+    }, 2000);*/
+});
+
+let counter = 0;
+client.on('data', (data) => {
+    console.log(data.toString());
+    counter++;
+});
+client.on('end', () => {
+    console.log(`${counter} events captured in ${Date.now() - startTime}`);
+    console.log('')
+    console.log('disconnected from server');
+});
