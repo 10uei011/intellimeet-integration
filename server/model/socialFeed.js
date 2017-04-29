bd9e16039fde295f68c5f09d66d89c9238284213
swati.jain <swati.jain@tothenew.com>
2017-04-29 12:23:32 +0530

    schema

---
+++ server/model/socialFeed.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const eventSchema = require('mongoose').Schema;
+
+const schema = new eventSchema({
+    name: { type: String, required: true },
+    createdAt: {type: Number, default: Date.now},
+    location: {
+        lat: { type: Number },
+        lng: { type: Number }
+    },
+    city: { type: String, required: true },
+    organisedBy: { type: String, required: true },
+    openEvent: { type: Number },
+    source: { type: String },
+    date: { type: Number },
+    charges: { type: String }
+});
+
+schema.index({createdAt: 1});
