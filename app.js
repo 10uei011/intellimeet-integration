dd079da4cd63b7f6c4211a05476ffda287257fab
swati.jain <swati.jain@tothenew.com>
2017-04-29 12:19:28 +0530

    integrating mongoose(without address)

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-var express = require('express')
+var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect('');
+
+mongoose.connection.on("error",(error) => {
+  console.log("######error#########",error);
+});
+
+mongoose.connection.on("connected",() => {
+  console.log("#######connected###########")
+});
 
 app.get('/', function (req, res) {
   res.send('Hello Integration !')
-})
+});
 
 app.listen(3000, function () {
   console.log('App is running on 3000!')
-})
+});
