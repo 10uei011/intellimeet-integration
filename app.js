81068615dd7a6a743623f3d4c33171e26f0efd6d
dipesh singh <dipesh.singh2012@gmail.com>
2017-04-29 12:45:54 +0530

    Edit app.js; Add mongourl

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-var express = require('express');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var app = express();
+const mongoUrl = require('./config/db');
 
-mongoose.connect('');
+mongoose.connect(mongoUrl.mongoUrl);
 
 mongoose.connection.on("error",(error) => {
   console.log("######error#########",error);
 });
 
 mongoose.connection.on("connected",() => {
-  console.log("#######connected###########")
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello Integration !')
-});
-
-app.listen(3000, function () {
-  console.log('App is running on 3000!')
+  console.log('connected to mongodb')
 });
